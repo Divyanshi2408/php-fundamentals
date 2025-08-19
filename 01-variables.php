55bc7aba480d07be0c7fd7aa650a766ced9418e3
Divyanshi pal <85859444+Divyanshi2408@users.noreply.github.com>
2025-08-19 11:42:02 +0530

    Create 01-variables.php

---
+++ 01-variables.php
@@ -0,0 +1,11 @@
+
+<?php
+  // Example of variables in PHP
+  $name = "John";
+  $city = "London";
+  $year = 2025;
+
+  echo "Hello, my name is $name.<br>";
+  echo "I live in $city.<br>";
+  echo "The year is $year.<br>";
+?>
